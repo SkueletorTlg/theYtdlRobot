92a99dce2f83b87059cb63977357eb84b3df392e
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 16:35:36 -0400

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -37,9 +37,6 @@ async def rename_doc(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D")
             chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
         return
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
