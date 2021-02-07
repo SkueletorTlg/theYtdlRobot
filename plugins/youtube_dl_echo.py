8de42e718b8d33d20f86b423b550b727cc6d7145
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 16:24:01 -0400

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -33,8 +33,8 @@ from helper_funcs.help_uploadbot import DownLoadFile
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
+    if update.from_user.id in Config.BANNED_USERS:
+        await update.reply_text("You are B A N N E D")
             chat_id=update.chat.id,
             message_ids=update.message_id,
             revoke=True
