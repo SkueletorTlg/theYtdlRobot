8c7d297e015f681b007a9933b6aab1e59e425513
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 19:50:01 -0400

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -31,9 +31,13 @@ from helper_funcs.display_progress import progress_for_pyrogram
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["generatescss"]))
 async def generate_screen_shot(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are B A N N E D")
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
             chat_id=update.chat.id,
+                      chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
         return
     TRChatBase(update.from_user.id, update.text, "generatescss")
     if update.reply_to_message is not None:
