e3892ce22f8b3400f91d8021ca9365986930364e
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 16:29:18 -0400

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -39,9 +39,6 @@ async def convert_to_video(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D")
             chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
         return
     TRChatBase(update.from_user.id, update.text, "converttovideo")
     if update.reply_to_message is not None:
