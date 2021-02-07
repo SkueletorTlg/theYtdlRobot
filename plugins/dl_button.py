7042649315dff25f802ab92f010642cf37cf014a
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 07:39:38 -0400

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -278,9 +278,9 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download
-URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+            text="""Iniciando descarga...
+Enlace: {}
+Tamaño del archivo: {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -299,11 +299,11 @@ File Size: {}""".format(url, humanbytes(total_length))
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                        current_message = """**Download Status**
-URL: {}
-File Size: {}
-Downloaded: {}
-ETA: {}""".format(
+                        current_message = """**Estado de la descarga**
+Enlace: {}
+Tamaño del archivo: {}
+Archivo descargado: {}
+Tiempo estimado: {}""".format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
