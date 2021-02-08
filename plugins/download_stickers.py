7bc7543e53d11247b17ce151da67a32978850ee6
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-07 20:32:43 -0400

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -32,7 +32,6 @@ async def DownloadStickersBot(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
-                      chat_id=update.chat.id,
             message_ids=update.message_id,
             revoke=True
         )
